f0fa8a477e1ea38fe8947411ed2ccf8ca0f69961
Mateusz Durmaj <m.durmaj@gmail.com>
2018-01-22 22:21:15 +0100

    created controller, entities and db
    using symfony generators

---
+++ airplane-manager/src/AppBundle/Entity/Airplane.php
@@ -0,0 +1,221 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Airplane
+ *
+ * @ORM\Table(name="airplane")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\AirplaneRepository")
+ */
+class Airplane
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="prodYear", type="integer")
+     */
+    private $prodYear;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="manufacturer", type="string", length=255)
+     */
+    private $manufacturer;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="model", type="string", length=255)
+     */
+    private $model;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="maxPassengers", type="integer")
+     */
+    private $maxPassengers;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="maxLoad", type="integer")
+     */
+    private $maxLoad;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Airplane
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set prodYear
+     *
+     * @param integer $prodYear
+     *
+     * @return Airplane
+     */
+    public function setProdYear($prodYear)
+    {
+        $this->prodYear = $prodYear;
+
+        return $this;
+    }
+
+    /**
+     * Get prodYear
+     *
+     * @return int
+     */
+    public function getProdYear()
+    {
+        return $this->prodYear;
+    }
+
+    /**
+     * Set manufacturer
+     *
+     * @param string $manufacturer
+     *
+     * @return Airplane
+     */
+    public function setManufacturer($manufacturer)
+    {
+        $this->manufacturer = $manufacturer;
+
+        return $this;
+    }
+
+    /**
+     * Get manufacturer
+     *
+     * @return string
+     */
+    public function getManufacturer()
+    {
+        return $this->manufacturer;
+    }
+
+    /**
+     * Set model
+     *
+     * @param string $model
+     *
+     * @return Airplane
+     */
+    public function setModel($model)
+    {
+        $this->model = $model;
+
+        return $this;
+    }
+
+    /**
+     * Get model
+     *
+     * @return string
+     */
+    public function getModel()
+    {
+        return $this->model;
+    }
+
+    /**
+     * Set maxPassengers
+     *
+     * @param integer $maxPassengers
+     *
+     * @return Airplane
+     */
+    public function setMaxPassengers($maxPassengers)
+    {
+        $this->maxPassengers = $maxPassengers;
+
+        return $this;
+    }
+
+    /**
+     * Get maxPassengers
+     *
+     * @return int
+     */
+    public function getMaxPassengers()
+    {
+        return $this->maxPassengers;
+    }
+
+    /**
+     * Set maxLoad
+     *
+     * @param integer $maxLoad
+     *
+     * @return Airplane
+     */
+    public function setMaxLoad($maxLoad)
+    {
+        $this->maxLoad = $maxLoad;
+
+        return $this;
+    }
+
+    /**
+     * Get maxLoad
+     *
+     * @return int
+     */
+    public function getMaxLoad()
+    {
+        return $this->maxLoad;
+    }
+}
+
---
+++ airplane-manager/src/AppBundle/Entity/Flight.php
@@ -0,0 +1,153 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use AppBundle\Entity\Airplane;
+
+/**
+ * Flight
+ *
+ * @ORM\Table(name="flight")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\FlightRepository")
+ */
+class Flight
+{
+    /**
+     * @ORM\ManyToOne(targetEntity="Airplane")
+     * @ORM\JoinColumn(name="airplane_id", referencedColumnName="id")
+     */
+    private $airplane;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="departAirport", type="string", length=255)
+     */
+    private $departAirport;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="arrivalAirport", type="string", length=255)
+     */
+    private $arrivalAirport;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="flightTime", type="integer")
+     */
+    private $flightTime;
+
+    // getters & setters
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set departAirport
+     *
+     * @param string $departAirport
+     *
+     * @return Flight
+     */
+    public function setDepartAirport($departAirport)
+    {
+        $this->departAirport = $departAirport;
+
+        return $this;
+    }
+
+    /**
+     * Get departAirport
+     *
+     * @return string
+     */
+    public function getDepartAirport()
+    {
+        return $this->departAirport;
+    }
+
+    /**
+     * Set arrivalAirport
+     *
+     * @param string $arrivalAirport
+     *
+     * @return Flight
+     */
+    public function setArrivalAirport($arrivalAirport)
+    {
+        $this->arrivalAirport = $arrivalAirport;
+
+        return $this;
+    }
+
+    /**
+     * Get arrivalAirport
+     *
+     * @return string
+     */
+    public function getArrivalAirport()
+    {
+        return $this->arrivalAirport;
+    }
+
+    /**
+     * Set flightTime
+     *
+     * @param integer $flightTime
+     *
+     * @return Flight
+     */
+    public function setFlightTime($flightTime)
+    {
+        $this->flightTime = $flightTime;
+
+        return $this;
+    }
+
+    /**
+     * Get flightTime
+     *
+     * @return int
+     */
+    public function getFlightTime()
+    {
+        return $this->flightTime;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAirplane()
+    {
+        return $this->airplane;
+    }
+
+    /**
+     * @param mixed $airplane
+     */
+    public function setAirplane($airplane): void
+    {
+        $this->airplane = $airplane;
+    }
+
+}
+
